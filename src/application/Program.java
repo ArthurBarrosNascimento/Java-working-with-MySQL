69946d9dc449035e950aca56f730df9ffc6b582c
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-23 18:42:37 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import db.DB;
 import db.DbException;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,9 +12,9 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = new Seller(31, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
