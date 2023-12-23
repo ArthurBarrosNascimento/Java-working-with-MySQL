302905dc8badee5bd3cd801fe4a12304ab40382d
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-23 18:09:22 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,13 +3,17 @@ package application;
 import db.DB;
 import db.DbException;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.*;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(31, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
