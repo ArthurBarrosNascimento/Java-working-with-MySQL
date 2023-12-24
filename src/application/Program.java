b601b4734e8baa925fd1654c1506b69f56609976
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-12-23 21:32:05 -0300

    delete implementation
    C.R.U.D Complete

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import db.DbException;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -7,38 +8,52 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("=== TEST 1: seller findById ===");
-        Seller seller = sellerDao.findById(3);
-        System.out.println(seller);
-        
-        System.out.println("\n=== TEST 2: seller findByDepartment ===");
-        Department department = new Department(2, null);
-        List<Seller> list = sellerDao.findByDepartment(department);
-        for (Seller obj: list) {
-            System.out.println(obj);
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("=== TEST 1: seller findById ===");
+            Seller seller = sellerDao.findById(3);
+            System.out.println(seller);
+
+            System.out.println("\n=== TEST 2: seller findByDepartment ===");
+            Department department = new Department(2, null);
+            List<Seller> list = sellerDao.findByDepartment(department);
+            for (Seller obj: list) {
+                System.out.println(obj);
+            }
+
+            System.out.println("\n=== TEST 3: seller findAll ===");
+            list = sellerDao.findAll();
+            for (Seller obj: list) {
+                System.out.println(obj);
+            }
+
+            System.out.println("\n=== TEST 4: seller insert ===");
+            Seller newSeller = new Seller(null, "Arthur", "arthur@gmail.com", new Date(), 4000.0, department);
+            sellerDao.insert(newSeller);
+            System.out.println("Inserted! new id = " + newSeller.getId());
+
+            System.out.println("\n=== TEST 5: seller update ===");
+            seller = sellerDao.findById(1);
+            seller.setName("Francine Nascimento");
+            sellerDao.updade(seller);
+            System.out.println("Update completed!");
+
+            System.out.println("\n=== TEST 6: seller delete ===");
+            System.out.print("Enter id for delete test: ");
+            int id = sc.nextInt();
+            sellerDao.deleteById(id);
+            System.out.println("Delete completed!");
+
+        } catch (DbException e) {
+            System.out.println(e.getMessage());
         }
-
-        System.out.println("\n=== TEST 3: seller findAll ===");
-        list = sellerDao.findAll();
-        for (Seller obj: list) {
-            System.out.println(obj);
-        }
-
-        System.out.println("\n=== TEST 4: seller insert ===");
-        Seller newSeller = new Seller(null, "Arthur", "arthur@gmail.com", new Date(), 4000.0, department);
-        sellerDao.insert(newSeller);
-        System.out.println("Inserted! new id = " + newSeller.getId());
-
-        System.out.println("\n=== TEST 5: seller update ===");
-        seller = sellerDao.findById(1);
-        seller.setName("Marta Wayne");
-        sellerDao.updade(seller);
-        System.out.println("Update completed!");
 
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -85,7 +85,21 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+            st.setInt(1, id);
 
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected == 0) {
+                throw new DbException("Error: id does not exist");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
